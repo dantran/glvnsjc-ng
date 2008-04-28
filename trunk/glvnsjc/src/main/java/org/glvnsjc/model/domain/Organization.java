3906e026a287c5ba4bac4d9713c810ebffe0381f
dantran <dantran@9b5e0c07-7f4b-0410-8dab-d3373bbaab2d>
2008-04-28 06:52:44 +0000

    Initial creation

---
+++ trunk/glvnsjc/src/main/java/org/glvnsjc/model/domain/Organization.java
@@ -0,0 +1,12 @@
+package org.glvnsjc.model.domain;
+
+import javax.persistence.Entity;
+
+import org.glvnsjc.model.ManagedObject;
+
+@Entity
+public class Organization
+    extends ManagedObject
+{
+    private static final long serialVersionUID = 1L;
+}
