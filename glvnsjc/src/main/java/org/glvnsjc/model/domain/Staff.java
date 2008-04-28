7824d506601f5525f0a73ca5dea9ce344377f06e
dantran <dantran@9b5e0c07-7f4b-0410-8dab-d3373bbaab2d>
2008-04-28 06:52:44 +0000

    Initial creation

---
+++ glvnsjc/src/main/java/org/glvnsjc/model/domain/Staff.java
@@ -0,0 +1,9 @@
+package org.glvnsjc.model.domain;
+
+import org.glvnsjc.model.ManagedObject;
+
+public class Staff
+    extends ManagedObject
+{
+    private static final long serialVersionUID = 1L;
+}
