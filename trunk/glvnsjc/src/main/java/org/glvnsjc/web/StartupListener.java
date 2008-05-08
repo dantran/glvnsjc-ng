4ef72dc81e83498c0b299b470315262ea78030aa
dantran <dantran@9b5e0c07-7f4b-0410-8dab-d3373bbaab2d>
2008-05-08 22:19:49 +0000

    add dummy startup listener and filter.

---
+++ trunk/glvnsjc/src/main/java/org/glvnsjc/web/StartupListener.java
@@ -0,0 +1,29 @@
+package org.glvnsjc.web;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+/**
+ * Bootstrapping startup configurations are here 
+ * @author dtran
+ *
+ */
+public class StartupListener
+    implements ServletContextListener
+{
+
+    @Override
+    public void contextDestroyed( ServletContextEvent sce )
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void contextInitialized( ServletContextEvent sce )
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+}
