8d5ccd0d0cf520f512f93f64361959074d3cd26b
rick.kull <rick.kull@9b5e0c07-7f4b-0410-8dab-d3373bbaab2d>
2008-05-08 00:31:01 +0000

    Attempt to get injection to work.  Uses TestView.

---
+++ trunk/glvnsjc/src/main/java/org/glvnsjc/web/TestView.java
@@ -0,0 +1,13 @@
+package org.glvnsjc.web;
+
+public class TestView
+{
+    
+    private String testString = "TestString";
+    
+    public String getTestString()
+    {
+        return this.testString;
+    }
+
+}
