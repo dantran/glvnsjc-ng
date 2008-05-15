a49d97acd8337622f2487e21cfd488c927c1b23a
dantran <dantran@9b5e0c07-7f4b-0410-8dab-d3373bbaab2d>
2008-05-15 01:07:08 +0000

    add Parent and Staff user type

---
+++ trunk/glvnsjc/src/main/java/org/glvnsjc/model/domain/Staff.java
@@ -0,0 +1,17 @@
+package org.glvnsjc.model.domain;
+
+import javax.persistence.Entity;
+
+/**
+ * None Academic User
+ * @author dtran
+ *
+ */
+
+@Entity
+public class Staff
+    extends User
+{
+    private static final long serialVersionUID = 1L;
+    
+}
--- trunk/glvnsjc/src/main/java/org/glvnsjc/model/domain/Student.java
+++ trunk/glvnsjc/src/main/java/org/glvnsjc/model/domain/Student.java
@@ -1,12 +1,26 @@
 package org.glvnsjc.model.domain;
 
 import javax.persistence.Entity;
+import javax.persistence.OneToOne;
 
 @Entity
 public class Student
     extends User
-
 {
     private static final long serialVersionUID = 1L;
+    
+    @OneToOne
+    private Parent parent;
+
+    public Parent getParent()
+    {
+        return parent;
+    }
+
+    public void setParent( Parent parent )
+    {
+        this.parent = parent;
+    }
+    
 
 }
