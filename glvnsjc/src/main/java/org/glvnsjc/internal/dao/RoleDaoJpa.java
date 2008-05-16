c0532f16b618659b79c296f47632520e6be8965d
dantran <dantran@9b5e0c07-7f4b-0410-8dab-d3373bbaab2d>
2008-05-16 22:12:54 +0000

    port over appfuse' RoleDao

---
+++ glvnsjc/src/main/java/org/glvnsjc/internal/dao/RoleDaoJpa.java
@@ -0,0 +1,48 @@
+package org.glvnsjc.internal.dao;
+
+import java.util.List;
+
+import javax.persistence.Query;
+
+import org.glvnsjc.internal.dao.generic.GenericNameDaoJpa;
+import org.glvnsjc.model.domain.Role;
+
+/**
+ * This class interacts with Spring's HibernateTemplate to save/delete and
+ * retrieve Role objects.
+ *
+ * @author <a href="mailto:bwnoll@gmail.com">Bryan Noll</a> 
+ */
+public class RoleDaoJpa
+    extends GenericNameDaoJpa<Role, Long> implements RoleDao {
+
+    /**
+     * Constructor to create a Generics-based version using Role as the entity
+     */
+    public RoleDaoJpa() {
+        super(Role.class);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public Role getRoleByName(String rolename) {
+        Query q = super.entityManager.createQuery("select r from Role r where r.name = ?");
+        q.setParameter(1, rolename);
+        List roles = q.getResultList();
+
+        if (roles.isEmpty()) {
+            return null;
+        } else {
+            return (Role) roles.get(0);
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void removeRole(String rolename) {
+        Object role = getRoleByName(rolename);
+        super.entityManager.remove(role);
+    }
+}
