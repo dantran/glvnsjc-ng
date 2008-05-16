01790cdbbc8cc31488d0863c0c41789afba27a2b
dantran <dantran@9b5e0c07-7f4b-0410-8dab-d3373bbaab2d>
2008-05-16 22:12:54 +0000

    port over appfuse' RoleDao

---
+++ trunk/glvnsjc/src/main/java/org/glvnsjc/internal/dao/RoleDao.java
@@ -0,0 +1,25 @@
+package org.glvnsjc.internal.dao;
+
+import org.glvnsjc.internal.dao.generic.GenericNameDao;
+import org.glvnsjc.model.domain.Role;
+
+/**
+ * Role Data Access Object (DAO) interface.
+ *
+ * @author <a href="mailto:matt@raibledesigns.com">Matt Raible</a>
+ */
+public interface RoleDao
+    extends GenericNameDao<Role, Long> {
+    /**
+     * Gets role information based on rolename
+     * @param rolename the rolename
+     * @return populated role object
+     */
+    Role getRoleByName(String rolename);
+
+    /**
+     * Removes a role from the database by name
+     * @param rolename the role's rolename
+     */
+    void removeRole(String rolename);
+}
---
+++ trunk/glvnsjc/src/main/java/org/glvnsjc/service/UserManager.java
@@ -0,0 +1,6 @@
+package org.glvnsjc.service;
+
+public interface UserManager
+{
+
+}
