fd17724997abf5a8be5d2f9da645cb6cae716339
dantran <dantran@9b5e0c07-7f4b-0410-8dab-d3373bbaab2d>
2008-05-19 14:36:51 +0000

    add RoleType enum

---
+++ trunk/glvnsjc/src/main/java/org/glvnsjc/model/domain/RoleType.java
@@ -0,0 +1,16 @@
+package org.glvnsjc.model.domain;
+
+public enum RoleType {
+    
+    ROLE_SYSTEM_ADMIN,
+    
+    ROLE_SCHOOL_ADMIN,
+    
+    ROLE_BRANCH_ADMIN,
+    
+    ROLE_TEACHER,
+    
+    ROLE_STUDENT,
+    
+    ROLE_PARENT
+}
