01bfb6203814f9df9e98b9939d0ed7d4c2de0cce
dantran <dantran@9b5e0c07-7f4b-0410-8dab-d3373bbaab2d>
2008-05-20 03:27:16 +0000

    properly shutdown hsqldb

---
+++ trunk/glvnsjc/src/main/java/org/glvnsjc/service/RoleManager.java
@@ -0,0 +1,36 @@
+package org.glvnsjc.service;
+
+import java.util.List;
+
+import org.glvnsjc.model.domain.Role;
+import org.glvnsjc.service.generic.UniversalManager;
+
+/**
+ * Business Service Interface to handle communication between web and
+ * persistence layer.
+ *
+ * @author <a href="mailto:dan@getrolling.com">Dan Kibler </a>
+ */
+public interface RoleManager
+    extends UniversalManager
+{
+    /**
+     * {@inheritDoc}
+     */
+    List getRoles( Role role );
+
+    /**
+     * {@inheritDoc}
+     */
+    Role getRole( String rolename );
+
+    /**
+     * {@inheritDoc}
+     */
+    Role saveRole( Role role );
+
+    /**
+     * {@inheritDoc}
+     */
+    void removeRole( String rolename );
+}
