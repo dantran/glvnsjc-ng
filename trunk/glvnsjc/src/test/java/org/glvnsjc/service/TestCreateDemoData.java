2329c7b063d560391bb6fab5a3c827c6705cff8b
dantran <dantran@9b5e0c07-7f4b-0410-8dab-d3373bbaab2d>
2008-05-23 16:15:16 +0000

    disable dao test, since it cannot run together with service test

---
+++ trunk/glvnsjc/src/test/java/org/glvnsjc/service/TestCreateDemoData.java
@@ -0,0 +1,17 @@
+package org.glvnsjc.service;
+
+import javax.annotation.Resource;
+
+import org.glvnsjc.service.demo.CreateDemoData;
+
+public class TestCreateDemoData
+    extends AbstractServiceTest
+{
+    @Resource
+    private CreateDemoData createSchoolDemo;
+    
+    public void testCreateDemoData()
+    {
+        
+    }
+}
