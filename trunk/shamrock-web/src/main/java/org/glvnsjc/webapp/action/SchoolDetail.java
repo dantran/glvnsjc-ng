610bf551085c4e63ea031a722f4abda3c16532ed
dantran <dantran@9b5e0c07-7f4b-0410-8dab-d3373bbaab2d>
2008-05-31 01:40:17 +0000

    breakup master detail school backing beans

---
+++ trunk/shamrock-web/src/main/java/org/glvnsjc/webapp/action/SchoolDetail.java
@@ -0,0 +1,106 @@
+package org.glvnsjc.webapp.action;
+
+import javax.annotation.Resource;
+import javax.faces.context.FacesContext;
+
+import org.glvnsjc.model.domain.School;
+import org.glvnsjc.service.SchoolManager;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope("request")
+public class SchoolDetail
+{
+
+    @Resource
+    private SchoolManager schoolManager;
+    
+    /////////////////////////////////////////////////////////////////////////////////////////
+
+    private Long selectedSchoolId;
+
+    private String selectedSchoolAction;
+
+    private School selectedSchool;
+   
+    ////////////////////////////////////////////////////////////////////////////////////////////
+    
+    public School getSelectedSchool()
+    {
+        if ( selectedSchoolId != null )
+        {
+            return this.selectedSchool;
+        }
+
+        //even though we don't render the school detail but still need a dummy school for loading purpose
+        return new School();
+    }
+
+    public void setSelectedSchool( School selectedSchool )
+    {
+        this.selectedSchool = selectedSchool;
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////
+    public Long getSelectedSchoolId()
+    {
+        return selectedSchoolId;
+    }
+
+    public void setSelectedSchoolId( Long selectedSchoolId )
+    {
+        this.selectedSchoolId = selectedSchoolId;
+        if ( selectedSchoolId.intValue() == 0 )
+        {
+            this.selectedSchool = new School();
+        }
+        else
+        {
+            this.selectedSchool = this.schoolManager.get( selectedSchoolId );
+        }
+
+    }
+    
+    ////////////////////////////////////////////////////////////////////////////////////////////
+
+    public String getSelectedSchoolAction()
+    {
+        return selectedSchoolAction;
+    }
+
+    public void setSelectedSchoolAction( String selectedSchoolAction )
+    {
+        this.selectedSchoolAction = selectedSchoolAction;
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////
+    
+    public boolean isRenderSelectedSchool()
+    {
+        return this.selectedSchoolId != null;
+    }
+    
+    public void prepareAdd( javax.faces.event.ActionEvent event )
+    {
+        this.selectedSchoolId = new Long( 0 );
+        this.selectedSchool = new School();
+    }
+
+
+    public boolean isReadOnly()
+    {
+        return "del".equals( this.selectedSchoolAction );
+    }
+    
+    public String prepareDetail( )
+    {
+        return null;//System.out.println( "Control Id: " + event.getComponent().getId() );
+    }
+
+    public void prepareDetail2( javax.faces.event.ActionEvent event )
+    {
+        System.out.println( "Prepare: " + event.getComponent().getClientId( FacesContext.getCurrentInstance() ) );
+    }
+    
+}
