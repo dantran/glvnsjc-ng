797047715fb99d3a9ccf40a9d028ea5ffa665f68
dantran <dantran@9b5e0c07-7f4b-0410-8dab-d3373bbaab2d>
2008-05-31 17:46:30 +0000

    add message utils

--- trunk/shamrock-web/src/main/java/org/glvnsjc/webapp/util/CrudType.java
+++ trunk/shamrock-web/src/main/java/org/glvnsjc/webapp/util/CrudType.java
@@ -1,10 +1,25 @@
 package org.glvnsjc.webapp.util;
 
+import org.apache.myfaces.shared.util.MessageUtils;
+
 public enum CrudType {
 
     ADD,
-    
+
     EDIT,
-    
-    DELETE
+
+    DELETE;
+
+    private static String BASE_BUNDLE = "org.glvnsjc.webapp.resource.ApplicationResources";
+
+    public static CrudType get( String message )
+    {
+        String bundleMsg = MessageUtils.getMessage( BASE_BUNDLE, "crud.remove", null ).toString();
+        if ( message.equals( bundleMsg ) )
+        {
+            return DELETE;
+        }
+
+        return null;
+    }
 }
---
+++ trunk/shamrock-web/src/main/java/org/glvnsjc/webapp/util/ExMessageUtils.java
@@ -0,0 +1,16 @@
+package org.glvnsjc.webapp.util;
+
+import org.apache.commons.lang.exception.ExceptionUtils;
+import org.apache.myfaces.shared.util.MessageUtils;
+
+
+
+/**
+ * Additional Message Utilities that not cover in Myfaces' shared's MessageUtils
+ * @author dtran
+ *
+ */
+public class ExMessageUtils
+{
+
+}
