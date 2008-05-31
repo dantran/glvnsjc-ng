91c6a504d1a8313b67e49b88d67008c5adda224a
dantran <dantran@9b5e0c07-7f4b-0410-8dab-d3373bbaab2d>
2008-05-31 01:40:17 +0000

    breakup master detail school backing beans

--- shamrock-web/src/main/java/org/glvnsjc/webapp/action/MainSchoolList.java
+++ shamrock-web/src/main/java/org/glvnsjc/webapp/action/MainSchoolList.java
@@ -2,7 +2,9 @@ package org.glvnsjc.webapp.action;
 
 import java.util.List;
 
+import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
+import javax.faces.context.FacesContext;
 
 import org.glvnsjc.model.domain.School;
 import org.glvnsjc.service.SchoolManager;
@@ -19,89 +21,23 @@ public class MainSchoolList
     private SchoolManager schoolManager;
     
     /////////////////////////////////////////////////////////////////////////////////////////
-
-    private Long selectedSchoolId;
-
-    private String selectedSchoolAction;
-
-    private School selectedSchool;
+    
+    private List<School> mainSchools = null;
     
     //////////////////////////////////////////////////////////////////////////////////////////
 
+    @PostConstruct
+    public void loadMainSchools()
+    {
+        //this.mainSchools = schoolManager.getMainSchools();
+    }
+    
+   
     public List<School> getMainSchools()
     {
         return schoolManager.getMainSchools();
+        //return this.mainSchools;
     }
 
-    public School getSelectedSchool()
-    {
-        if ( selectedSchoolId != null )
-        {
-            return this.selectedSchool;
-        }
-
-        //even though we don't render the school detail but still need a dummy school for loading purpose
-        return new School();
-    }
-
-    public boolean isRenderSelectedSchool()
-    {
-        return this.selectedSchoolId != null;
-    }
-
-    public Long getSelectedSchoolId()
-    {
-        return selectedSchoolId;
-    }
-
-    public void setSelectedSchoolId( Long selectedSchoolId )
-    {
-        this.selectedSchoolId = selectedSchoolId;
-        if ( selectedSchoolId.intValue() == 0 )
-        {
-            this.selectedSchool = new School();
-        }
-        else
-        {
-            this.selectedSchool = this.schoolManager.get( selectedSchoolId );
-        }
-
-    }
-
-    public String getSelectedSchoolAction()
-    {
-        return selectedSchoolAction;
-    }
-
-    public void setSelectedSchoolAction( String selectedSchoolAction )
-    {
-        this.selectedSchoolAction = selectedSchoolAction;
-    }
-
-    public void prepareAdd( javax.faces.event.ActionEvent event )
-    {
-        this.selectedSchoolId = new Long( 0 );
-        this.selectedSchool = new School();
-    }
-
-    public void processSelectedSchool( javax.faces.event.ActionEvent event )
-    {
-
-        //save the school
-
-        //reset the selectSchoolId to trigger the view to hide the school details block
-        this.selectedSchoolId = null;
-
-    }
-
-    public void setSelectedSchool( School selectedSchool )
-    {
-        this.selectedSchool = selectedSchool;
-    }
-
-    public boolean isReadOnly()
-    {
-        return "del".equals( this.selectedSchoolAction );
-    }
-
+    
 }
